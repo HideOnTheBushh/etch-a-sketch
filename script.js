c6f4c0c88fe5342104c86896d4064c387a671199
HideOnTheBushh <tonyzika@seznam.cz>
2024-12-16 13:34:28 +0100

    Add Change size button

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 let container = document.querySelector(".container");
 
+
 CreateDivs();
 
-let boxes = document.querySelectorAll(".box");
-boxes.forEach(function(elem) {
-    elem.addEventListener("mouseenter", function() {
-        elem.style.backgroundColor = "blue";
-    })
+
+// Change the size of the grid
+change = document.querySelector("#change")
+
+change.addEventListener("click", function() {
+
+    let n;
+    
+    // Keep prompting the user until a valid input is provided
+    while (true) {
+        // Prompt the user for a value
+        n = prompt("Enter the size of the grid. Maximum is 100.");
+
+        // Convert the input to an integer
+        n = parseInt(n, 10);
+
+        // Check if the input is a valid integer between 1 and 100
+        if (Number.isInteger(n) && n >= 1 && n <= 100) {
+            break;  // Exit the loop if the input is valid
+        } else {
+            alert("Please enter a valid number between 1 and 100.");
+        }
+    }
+
+    // Change the grid
+    CreateDivs(n);
 })
-
-
-
-
-
 
 
 
@@ -27,6 +44,7 @@ boxes.forEach(function(elem) {
 
 // Creates n*n divs in a square
 function CreateDivs (n=16) {
+    container.innerHTML = "";
 
     // Create n columns
     for (let i = 0; i < n; i++) {
@@ -47,4 +65,14 @@ function CreateDivs (n=16) {
         // Append to the container
         container.appendChild(column);
     }
+
+    // Changes boxes background color when hover over them
+    let boxes = document.querySelectorAll(".box");
+
+    boxes.forEach(function(elem) {
+        elem.addEventListener("mouseenter", function() {
+            elem.style.backgroundColor = "blue";
+        })
+    })
+
 }
